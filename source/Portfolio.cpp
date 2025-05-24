0520f0892407419bfacace9be8997160e84cb6d8
jitbarui1720 <j.barui@iitg.ac.in>
2025-05-24 16:13:06 +0530

    Update Portfolio.cpp

--- source/Portfolio.cpp
+++ source/Portfolio.cpp
@@ -1,9 +1,35 @@
 #include "Portfolio.h"
 
-void Portfolio::setEquity(double amount) {
-    equity = amount;
+Portfolio::Portfolio(double initialCash) : cash(initialCash), holdings(0) {}
+
+double Portfolio::getCash() const {
+    return cash;
 }
 
-double Portfolio::getEquity() const {
-    return equity;
+int Portfolio::getHoldings() const {
+    return holdings;
+}
+
+double Portfolio::getEquity(double currentPrice) const {
+    return cash + (holdings * currentPrice);
+}
+
+void Portfolio::increaseCash(double amount) {
+    cash += amount;
+}
+
+void Portfolio::decreaseCash(double amount) {
+    if (amount <= cash) {
+        cash -= amount;
+    }
+}
+
+void Portfolio::increaseHoldings(int units) {
+    holdings += units;
+}
+
+void Portfolio::decreaseHoldings(int units) {
+    if (units <= holdings) {
+        holdings -= units;
+    }
 }

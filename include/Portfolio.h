b67d17fae899419323f8c37a66fcc43fd957c2c8
jitbarui1720 <j.barui@iitg.ac.in>
2025-05-24 16:12:04 +0530

    Update Portfolio.h

--- include/Portfolio.h
+++ include/Portfolio.h
@@ -1,10 +1,18 @@
 #pragma once
-
 class Portfolio {
 private:
-    double equity;
+    double cash;
+    int holdings;
 
 public:
-    void setEquity(double amount);
-    double getEquity() const;
+    Portfolio(double initialCash = 10000.0);
+
+    double getCash() const;
+    int getHoldings() const;
+    double getEquity(double currentPrice) const;
+
+    void increaseCash(double amount);
+    void decreaseCash(double amount);
+    void increaseHoldings(int units);
+    void decreaseHoldings(int units);
 };
